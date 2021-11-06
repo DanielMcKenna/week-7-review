c29df7c1ed52fe3857c8108c75501ae9288307de
Daniel McKenna <mckenna.daniel15@gmail.com>
2021-11-06 09:37:05 -0600

    completed the review

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const alertUserDetails = (event) => {
   // Select the email and password inputs and get their values
-
-  alert(`Email: ${email}\nPassword: ${password}`)
-}
+  event.preventDefault();
+  alert(
+    `Email: ${event.target.email.value}\nPassword: ${event.target.password.value}`
+  );
+};
 
 // Select form element
 
+const logInForm = document.querySelector("#login-form");
+
 // Add an event listener that will use the 'alertUserDetails' function defined above as the callback
+
+logInForm.addEventListener("submit", alertUserDetails);
