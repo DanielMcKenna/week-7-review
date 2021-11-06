4425ef704422e7dd8154aaf05192c64b174b2ef2
John Redd <john.redd@endevvr.io>
2021-11-06 08:59:11 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const alertUserDetails = (event) => {
+  // Select the email and password inputs and get their values
+
+  alert(`Email: ${email}\nPassword: ${password}`)
+}
+
+// Select form element
+
+// Add an event listener that will use the 'alertUserDetails' function defined above as the callback
